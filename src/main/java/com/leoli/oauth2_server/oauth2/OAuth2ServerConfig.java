0a6a7cf1abd69bcf4c57728abaaa02b472c895c3
leoli <936339745@qq.com>
2019-03-20 00:49:55 +0800

    修改一些配置

--- src/main/java/com/leoli/oauth2_server/oauth2/OAuth2ServerConfig.java
+++ src/main/java/com/leoli/oauth2_server/oauth2/OAuth2ServerConfig.java
@@ -8,7 +8,6 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.MessageDigestPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
@@ -54,8 +53,7 @@ public class OAuth2ServerConfig extends
 
     @Bean
     public TokenStore tokenStore() {
-        RedisTokenStore redis = new RedisTokenStore(connectionFactory);
-        return redis;
+        return new RedisTokenStore(connectionFactory);
     }
 
     @Override
@@ -67,12 +65,18 @@ public class OAuth2ServerConfig extends
                 .secret("{bcrypt}" + passwordEncoder.encode("123456"))
                 .scopes("restapi")
                 .authorizedGrantTypes("password","authorization_code", "refresh_token")
-                .redirectUris("http://baidu.com");
+                .redirectUris("http://authtest.com") //authorization_code模式需要
+                .resourceIds();
     }
 
     @Override
     public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
-        security.tokenKeyAccess("permitAll()").checkTokenAccess("isAuthenticated()").allowFormAuthenticationForClients();
+        //tokenKeyAccess是公钥的访问权限，和jw一起使用， checkTokenAccess是check token access，两个的默认值都是denyAll()"
+//        security.tokenKeyAccess("permitAll()").checkTokenAccess("isAuthenticated()").allowFormAuthenticationForClients();
+
+        // allowFormAuthenticationForClients==true时在AuthorizationServerSecurityConfigurer的config中添加一个filter
+        // 这个filter时验证client的，不设置为true会获取不了token
+        security.allowFormAuthenticationForClients();
     }
 
 }
