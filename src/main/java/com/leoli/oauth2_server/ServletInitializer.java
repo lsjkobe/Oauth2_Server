32f9188511245540f26591421afc84111ad3ba2c
leoli <936339745@qq.com>
2019-03-22 14:55:29 +0800

    注释

--- src/main/java/com/leoli/oauth2_server/ServletInitializer.java
+++ src/main/java/com/leoli/oauth2_server/ServletInitializer.java
@@ -3,6 +3,8 @@ package com.leoli.oauth2_server;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+
+//添加这个类是因为tomcat部署的问题，不添加访问会404
 public class ServletInitializer extends SpringBootServletInitializer {
 
     @Override
